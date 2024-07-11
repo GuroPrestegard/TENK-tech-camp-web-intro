3dae0f4260a309de6a491119f6d6d01129bcca3c
Guro Prestegard <guro.prestegard@knowit.no>
2024-07-11 14:42:13 +0200

    setup deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const options = {
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://astro-tech-blog-ten.vercel.app/',
+        site: 'https://guroprestegard.github.io',
 	markdown: {
 		syntaxHighlight: false,
 		// Disable syntax built-in syntax hightlighting from astro
